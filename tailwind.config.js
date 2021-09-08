5c5a9d4cdf4816c82404296974d04dd35033cc06
Martin Kruger <martin@martinkruger.com>
2021-09-08 07:34:19 +0200

    Starting point

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  purge: {
+    enabled: false,
+    content: ['./src/**/*.js', './src/**/*.jsx', './public/index.html'],
+  },
+  theme: {},
+  variants: {},
+  plugins: [],
+};
